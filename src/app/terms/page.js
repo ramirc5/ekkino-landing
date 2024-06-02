a49bfd745acccf6701ff37bce3430c6a5e59207c
Cristian <125297328+ramirc5@users.noreply.github.com>
2024-06-02 12:33:49 -0700

    fix

--- src/app/terms/page.js
+++ src/app/terms/page.js
@@ -27,10 +27,10 @@ export default function Home() {
           <p>For the purposes of these Terms and Conditions:</p>
           <p>
             Affiliate means an entity that controls, is controlled by or is
-            under common control with a party, where "control" means ownership
-            of 50% or more of the shares, equity interest or other securities
-            entitled to vote for election of directors or other managing
-            authority.
+            under common control with a party, where &quot;control&quot; means
+            ownership of 50% or more of the shares, equity interest or other
+            securities entitled to vote for election of directors or other
+            managing authority.
           </p>
           <p>Country refers to: Wyoming, United States</p>
           <p>
@@ -136,8 +136,8 @@ export default function Home() {
             liability of the Company and any of its suppliers under any
             provision of this Terms and Your exclusive remedy for all of the
             foregoing shall be limited to the amount actually paid by You
-            through the Service or 100 USD if You haven't purchased anything
-            through the Service.
+            through the Service or 100 USD if You haven&apos;t purchased
+            anything through the Service.
           </p>
           <p>
             To the maximum extent permitted by applicable law, in no event shall
@@ -156,36 +156,37 @@ export default function Home() {
             Some states do not allow the exclusion of implied warranties or
             limitation of liability for incidental or consequential damages,
             which means that some of the above limitations may not apply. In
-            these states, each party's liability will be limited to the greatest
-            extent permitted by law.
+            these states, each party&apos;s liability will be limited to the
+            greatest extent permitted by law.
           </p>
           <h1 className="font-sans font-semibold text-[24px]">
-            "AS IS" and "AS AVAILABLE" Disclaimer of Liability{" "}
+            &quot;AS IS&quot; and &quot;AS AVAILABLE&quot; Disclaimer of
+            Liability{" "}
           </h1>
           <p>
-            The Service is provided to You "AS IS" and "AS AVAILABLE" and with
-            all faults and defects without warranty of any kind. To the maximum
-            extent permitted under applicable law, the Company, on its own
-            behalf and on behalf of its Affiliates and its and their respective
-            licensors and service providers, expressly disclaims all warranties,
-            whether express, implied, statutory or otherwise, with respect to
-            the Service, including all implied warranties of merchantability,
-            fitness for a particular purpose, title and non-infringement, and
-            warranties that may arise out of course of dealing, course of
-            performance, usage or trade practice. Without limitation to the
-            foregoing, the Company provides no warranty or undertaking, and
-            makes no representation of any kind that the Service will meet Your
-            requirements, achieve any intended results, be compatible or work
-            with any other software, applications, systems or services, operate
-            without interruption, meet any performance or reliability standards
-            or be error free or that any errors or defects can or will be
-            corrected.
+            The Service is provided to You &quot;AS IS&quot; and &quot;AS
+            AVAILABLE&quot; and with all faults and defects without warranty of
+            any kind. To the maximum extent permitted under applicable law, the
+            Company, on its own behalf and on behalf of its Affiliates and its
+            and their respective licensors and service providers, expressly
+            disclaims all warranties, whether express, implied, statutory or
+            otherwise, with respect to the Service, including all implied
+            warranties of merchantability, fitness for a particular purpose,
+            title and non-infringement, and warranties that may arise out of
+            course of dealing, course of performance, usage or trade practice.
+            Without limitation to the foregoing, the Company provides no
+            warranty or undertaking, and makes no representation of any kind
+            that the Service will meet Your requirements, achieve any intended
+            results, be compatible or work with any other software,
+            applications, systems or services, operate without interruption,
+            meet any performance or reliability standards or be error free or
+            that any errors or defects can or will be corrected.
           </p>
           <p>
             Without limiting the foregoing, neither the Company nor any of the
-            company's provider makes any representation or warranty of any kind,
-            express or implied: (i) as to the operation or availability of the
-            Service, or the information, content, and materials or products
+            company&apos;s provider makes any representation or warranty of any
+            kind, express or implied: (i) as to the operation or availability of
+            the Service, or the information, content, and materials or products
             included thereon; (ii) that the Service will be uninterrupted or
             error-free; (iii) as to the accuracy, reliability, or currency of
             any information or content provided through the Service; or (iv)
@@ -230,8 +231,8 @@ export default function Home() {
           <p>
             You represent and warrant that (i) You are not located in a country
             that is subject to the United States government embargo, or that has
-            been designated by the United States government as a "terrorist
-            supporting" country, and (ii) You are not listed on any United
+            been designated by the United States government as a &quot;terrorist
+            supporting&quot; country, and (ii) You are not listed on any United
             States government list of prohibited or restricted parties.
           </p>
           <h1 className="font-sans font-semibold text-[24px]">
@@ -249,7 +250,7 @@ export default function Home() {
           <p>
             Except as provided herein, the failure to exercise a right or to
             require performance of an obligation under these Terms shall not
-            affect a party's ability to exercise such right or require such
+            affect a party&apos;s ability to exercise such right or require such
             performance at any time thereafter nor shall the waiver of a breach
             constitute a waiver of any subsequent breach.
           </p>
@@ -267,9 +268,9 @@ export default function Home() {
           <p>
             We reserve the right, at Our sole discretion, to modify or replace
             these Terms at any time. If a revision is material We will make
-            reasonable efforts to provide at least 30 days' notice prior to any
-            new terms taking effect. What constitutes a material change will be
-            determined at Our sole discretion.
+            reasonable efforts to provide at least 30 days&apos; notice prior to
+            any new terms taking effect. What constitutes a material change will
+            be determined at Our sole discretion.
           </p>
           <p>
             By continuing to access or use Our Service after those revisions
